aa4de97478d7fd9f398a842efb96831cc5f585af
Nicholas Kristoffersen <nick@kristoffersen.io>
2017-11-08 15:18:01 +0100

    updating api url

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -8,20 +8,20 @@
  * Service in the appredomaycomApp.
  */
 angular.module('appredomaycomApp')
-	.service('api', function ($http) {
-		var urlBase = 'https://api.kristoffersen.io/api';
+  .service('api', function ($http) {
+    var urlBase = 'https://api.jobhero.org/api';
 
-		this.getResume = function (id) {
-			return $http.get(urlBase + '/resume/' + id);
-		};
+    this.getResume = function (id) {
+      return $http.get(urlBase + '/resume/' + id);
+    };
 
-		this.postResume = function (resume) {
-			return $http.post(urlBase + '/resume', resume);
-		};
+    this.postResume = function (resume) {
+      return $http.post(urlBase + '/resume', resume);
+    };
 
-		this.updateResume = function (resumeId, resume) {
-			console.log('api.updateResume', resumeId, resume);
-			return $http.put(urlBase + '/resume/' + resumeId, resume);
-		};
+    this.updateResume = function (resumeId, resume) {
+      console.log('api.updateResume', resumeId, resume);
+      return $http.put(urlBase + '/resume/' + resumeId, resume);
+    };
 
-	});
+  });
